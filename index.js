286a448ad2c75958818326d3592eb299e678b9ce
Edward Salinas <edwsalr.es@gmail.com>
2025-05-30 19:30:43 -0400

    seccion 23 finalizada

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors')
+const { dbConnection } = require('./database/config');
+// console.log(process.env);
+
+
+//Crear el servidor de express
+const app = express();
+
+//Base de datos
+dbConnection();
+
+// CORS
+app.use( cors() )
+
+//Directorio Publico
+app.use( express.static('public') )
+
+// Lectura y parseo del body
+app.use( express.json() );
+
+//Rutas
+app.use('/api/auth', require('./routes/auth'));
+// TODO CRUD: Eventos
+
+//Escuchar Peticiones
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+    
+})
